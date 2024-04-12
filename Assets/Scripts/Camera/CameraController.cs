a51cd2e2db15b1b8c658223757710bc2bb35bafd
Entoni1122 <entonishkalla34@gmail.com>
2024-04-12 13:39:36 +0200

    Minor Update to camera zoom

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -11,7 +11,7 @@ namespace TowerDefense
         [SerializeField] float cameraSpeedRotation;
         [SerializeField] float cameraZoomSpeed;
         [SerializeField] float borderTreshold;
-        float targetFOV = 80;
+        float targetFOV = 60;
         bool bShouldCameraMove;
 
         [Space]
@@ -26,66 +26,64 @@ namespace TowerDefense
         }
         void Update()
         {
-            CalculateCameraMovement();
-            if (Input.GetKeyDown(KeyCode.P))
-            {
-                bShouldCameraMove = !bShouldCameraMove;
-            }
-            CameraZoom();
-            CalculateRotation();
-        }
-        void CalculateCameraMovement()
-        {
-            if (bShouldCameraMove)
-            {
-                Direction = Vector3.zero;
-                if (Input.GetKey(KeyCode.W))
-                {
-                    Direction.z = +1;
-                }
-                if (Input.GetKey(KeyCode.S))
-                {
-                    Direction.z = -1;
-                }
-                if (Input.GetKey(KeyCode.D))
-                {
-                    Direction.x = +1;
-                }
-                if (Input.GetKey(KeyCode.A))
-                {
-                    Direction.x = -1;
-                }
+            //CalculateCameraMovement();
+            //if (Input.GetKeyDown(KeyCode.P))
+            //{
+            //    bShouldCameraMove = !bShouldCameraMove;
+            //}
+            //if (Input.GetKeyDown(KeyCode.L))
+            //{
+            //    CameraZoom();
+            //}
 
-                Vector3 moveCamera = transform.forward * Direction.z + transform.right * Direction.x;
-                transform.position += moveCamera * cameraSpeed * Time.deltaTime;
-            }
+            //CalculateRotation();
         }
-        void CalculateRotation()
-        {
-            float rotatedir = 0;
-            if (Input.GetKey(KeyCode.Q))
-            {
-                rotatedir += 1;
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                rotatedir -= 1;
-            }
-            transform.eulerAngles += new Vector3(0, rotatedir * cameraSpeedRotation * Time.deltaTime, 0);
-        }
-        void CameraZoom()
-        {
-            if (Input.mouseScrollDelta.y > 0)
-            {
-                targetFOV -= 5f;
-            }
-            if (Input.mouseScrollDelta.y < 0)
-            {
-                targetFOV += 5f;
-            }
-            targetFOV = Mathf.Clamp(targetFOV, 15, 60);
+        //void CalculateCameraMovement()
+        //{
+        //    if (bShouldCameraMove)
+        //    {
+        //        Direction = Vector3.zero;
+        //        if (Input.GetKey(KeyCode.W))
+        //        {
+        //            Direction.z = +1;
+        //        }
+        //        if (Input.GetKey(KeyCode.S))
+        //        {
+        //            Direction.z = -1;
+        //        }
+        //        if (Input.GetKey(KeyCode.D))
+        //        {
+        //            Direction.x = +1;
+        //        }
+        //        if (Input.GetKey(KeyCode.A))
+        //        {
+        //            Direction.x = -1;
+        //        }
 
-            CameraRef.fieldOfView = Mathf.Lerp(CameraRef.fieldOfView, targetFOV, cameraZoomSpeed * Time.deltaTime);
+        //        Vector3 moveCamera = transform.forward * Direction.z + transform.right * Direction.x;
+        //        transform.position += moveCamera * cameraSpeed * Time.deltaTime;
+        //    }
+        //}
+        //void CalculateRotation()
+        //{
+        //    float rotatedir = 0;
+        //    if (Input.GetKey(KeyCode.Q))
+        //    {
+        //        rotatedir += 1;
+        //    }
+        //    if (Input.GetKey(KeyCode.E))
+        //    {
+        //        rotatedir -= 1;
+        //    }
+        //    transform.eulerAngles += new Vector3(0, rotatedir * cameraSpeedRotation * Time.deltaTime, 0);
+        //}
+        bool shouldZoom;
+        public void CameraZoom()
+        {
+            targetFOV = shouldZoom ? targetFOV = 60 : 90;
+
+            CameraRef.fieldOfView = targetFOV;
+            shouldZoom = !shouldZoom;
         }
     }
 }
